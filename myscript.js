0cbd98c564e257583603296ae06b77bea9ef4c40
stallone1 <66884574+stallone1@users.noreply.github.com>
2022-10-05 20:33:54 +0100

    Add files via upload
    this project includes vanilla javascript

---
+++ myscript.js
@@ -0,0 +1,28 @@
+ 
+
+
+document.querySelector('button').addEventListener('click', function (){
+alert(' your vote has been successful!');
+});
+
+
+
+let adnBtn = document.querySelectorAll('.dot').length;
+
+for (let i = 0; i < adnBtn; i++) {
+document.querySelectorAll('.dot')[i].addEventListener('click', function () {
+alert('thanks for rating!');
+});
+
+}
+
+
+
+
+ 
+
+
+
+
+
+
